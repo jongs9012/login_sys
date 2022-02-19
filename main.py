b612c26424a9ed772d8dee0c4860a57116e05f62
Jong's <77009348+jongs9012@users.noreply.github.com>
2022-02-20 02:59:59 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,113 @@
-
+from time import sleep
 import streamlit as st
-from streamlit.script_runner import RerunException
-from streamlit.script_request_queue import RerunData
 import datetime
 
+st.set_page_config(layout='wide', )
 
-# When the btn pressed, Add some values to notice it clicked
-# So it can draw some interface to interact with client
+def avaliable_account(id, pwd):
+    return True
 
-st.title("WELCOME")
-logined = False
 
-if logined == False:
-    if st.button("Sign in"):
-        st.subheader("Log in to see fancy datas!")
-        userID = str(st.text_input("Enter ID"))
-        userPWD = str(st.text_input("Enter Password", type="password"))
-        if st.button("Log in"):
-            # Sleeping Day or Night ?
-            st.subheader("Are you sleeping at Night or Day ?")
-            nightOrDay = st.radio("Night or Day", ("Day", "Night"))
 
-            # Sleeping Hours
-            st.subheader(f"How many hours you sleep at {nightOrDay}?")
-            sleep_hours = st.radio("Hours you sleep", ("4 ~ 5 hours", "5 ~ 6 hours", "6 ~ 7 hours", "Others"))
+# User survey
+def survey():
+    # Sleeping Day or Night ?
+    st.subheader("Are you sleeping at Night or Day ?", anchor=False)
+    nightOrDay = st.radio("Night or Day", ("Day", "Night"))
+    # Sleeping Hours
+    st.subheader(f"How many hours you sleep at {nightOrDay}?")
+    sleep_hours = st.radio("Hours you sleep", ("4 ~ 5 hours", "5 ~ 6 hours", "6 ~ 7 hours", "Others"))
+    # Reason about sleeping that hours
+    temp = "Why do you sleep not normally?" if sleep_hours == "Others" else f"Why do you sleeping {sleep_hours}"
+    st.subheader(temp)
+    reason = str(st.text_input("Please enter reason"))
+    if len(reason) >= 1:
+        onceOrNot = st.radio(" ", ("Yes, I usually sleep once at a day.", "No, I'm not sleep once at a day."))
+        st.subheader("How many times a week do you dream ?")
+        dreamingTimes = st.radio("Times you dreaming", ("0 ~ 1 times", "2 ~ 3 times", "4 ~ 5 times", "Others"))
+        qualityDream = st.slider("If you were to grade your sleep?", 0, 100, 70)
+        st.write(f"[ Score : {qualityDream} ]")
+        temp = "Are you sleeping at Once?" if sleep_hours == "Others" else f"Are you sleeping {sleep_hours} at Once?"
+        st.subheader(temp)
+        lightState = st.radio("Do you sleep with the lights on?", ("Turn on", "Turn off"))
+        st.subheader("")
+        st.subheader("_______________________________")
+        st.write("Do you agree to provide information?")
+        st.write("It's not used for commercial purpose and is used for information analysis.")
+        submitBtn = st.button("Submit")
 
-            # Reason about sleeping that hours
-            temp = "Why do you sleep not normally?" if sleep_hours is "Others" else f"Why do you sleeping {sleep_hours}"
-            st.subheader(temp)
-            reason = str(st.text_input("Please enter reason"))
 
-            if len(reason) >= 1:
-                c1, c2 = st.columns(2)
-                with c1:
-                    tempInC1 = "Are you sleeping at Once?" if sleep_hours is "Others" else f"Are you sleeping {sleep_hours} at Once?"
-                    st.write(" ")
-                    st.subheader(tempInC1)
-                with c2:
-                    st.subheader(" ")
-                    onceOrNot = st.radio(" ",("Yes, I usually sleep once at a day.", "No, I'm not sleep once at a day."))
+def create_Account(name, id, password, time):
+    userfile = open(f"./users/{name}_info.txt", "w")
+    userfile.write(f"name:{name}\n")
+    userfile.write(f"ID:{id}\n")
+    userfile.write(f"PWD:{password}\n")
+    userfile.write(f"Sign_up_time:{time}")
+    userfile.close()
+    st.success("Sucessfully finished !")
+    st.write("Go to login menu to login.")
 
-    # SIGN UP
-    elif st.button("Sigh up"):
-        st.subheader("Thanks for joining us")
-        col1, col2 = st.columns(2)
-        with col1:  # Set username
-            st.header("Name")
-            usernameOnsignup = str(st.text_input("Enter your name"))
 
-        with col2:  # Set userID / userPWD
-            st.header("ID / PW")
-            useridOnSignup = str(st.text_input("Make wonderful ID"))
-            userpwdOnSighup = str(st.text_input("Ridiculas PWD is the best", type="password"))
-            if len(userpwdOnSighup) <= 8 and userpwdOnSighup != None:
-                st.info("We recommend you to make long password")
+def main():
+    # Title message
+    st.title("Welcome", anchor=False)
 
-        if (len(useridOnSignup) >= 2) and (len(userpwdOnSighup) >= 2) and (len(usernameOnsignup) >= 2):
-            if st.checkbox("Agree to save your info"):
+    # Sidebar Menu
+    menu = ["Main", "Login", "Sign Up"]
+    choose = st.sidebar.selectbox("Menu",menu)
+
+    # Main page
+    if choose == "Main":
+        st.subheader("We handle so many data that can make you surprise.")
+        st.subheader("Why don't you sign up to see all data?")
+
+
+    # Login page
+    if choose == "Login":
+        st.subheader("Sign in to see fancy datas!", anchor=False)
+        st.subheader("_______________________________")
+
+        userID = str(st.sidebar.text_input("Enter ID"))
+        userPWD = str(st.sidebar.text_input("Enter Password", type="password"))
+
+        if len(userID) >= 1 and len(userPWD) >= 1:
+            st.sidebar.subheader("Click the box to login \nIf click check box again you will logout")
+        loginCheckBox = st.sidebar.checkbox("Login")
+        tested = False  # Receive a value from userinfo.txt
+        if loginCheckBox:
+            if avaliable_account(userID, userPWD):
+                if tested == False:
+                    survey()
+                else:
+                    st.subheader("Passed")
+            # If user doesn't have survey data.
+            else:
+                st.sidebar.write("ID or Password is wrong")
+
+
+
+    # Sign up page
+    if choose == "Sign Up":
+        st.subheader("Thanks for joining us", anchor=False)
+        st.subheader("_______________________________")
+        st.header("Name")
+        userName = str(st.text_input("Enter your name"))
+        st.header("ID / PW", anchor=None)
+        userID = str(st.text_input("Make wonderful ID"))
+        userPWD = str(st.text_input("Ridicules password is the best", type="password"))
+        if len(userPWD) <= 8 and userPWD != None:
+            st.info("We recommend you to make long password")
+        if (len(userID) >= 2) and (len(userPWD) >= 2) and (len(userName) >= 2):
+            if st.checkbox("Agree to save my info"):
+                # Save and register user
                 if st.button("Sign UP!"):
-                    userfile = open(f"./{usernameOnsignup}_info.txt", "w")
-                    userfile.write(f"name : {usernameOnsignup}\n")
-                    userfile.write(f"ID : {useridOnSignup}\n")
-                    userfile.write(f"PWD : {userpwdOnSighup}\n")
-                    userfile.write(f"SighUP time : {datetime.datetime.now()}")
-                    userfile.close()
-                    logined = True
-                    st.experimental_rerun()
+                    # Make user file
+                    create_Account(userName, userID, userPWD, datetime.datetime.now())
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
